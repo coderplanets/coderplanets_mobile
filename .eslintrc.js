b331131e1998b06ba9a49ac5c9b6efea1f55493a
mydearxym <mydearxym@qq.com>
2019-06-21 14:58:18 +0800

    chore(infra): add wcandillon's eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// all rules details check: https://github.com/wcandillon/eslint-config-react-native-wcandillon/blob/master/index.js
+module.exports = {
+  extends: 'react-native-wcandillon',
+  rules: {
+    'react-native/no-color-literals': 0,
+    'import/no-extraneous-dependencies': 2,
+  },
+}
